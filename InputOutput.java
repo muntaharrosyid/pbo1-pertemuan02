704d6192c6a3603304faf0b37f5650d92ad7bd08
Muntahar Rosyid <165446937+muntaharrosyid@users.noreply.github.com>
2024-04-19 14:45:49 +0800

    Buat Output

--- InputOutput.java
+++ InputOutput.java
@@ -1,10 +1,10 @@
-import java.util.scanner;
+import java.util.Scanner;
 
 public class InputOutput{
  public static void main(String [] args){
   Scanner scanner = new Scanner(System.in);
   String nama;
-  System.out,print("Masukkan nama anda: ");
+  System.out.print("Masukkan nama anda: ");
   nama = scanner.nextline();
   System.out.println("Hai, "+nama);
  }
