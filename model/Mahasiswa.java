3b9cf783d949ad3aa0520f75f207c049f867488a
Muntahar Rosyid <165446937+muntaharrosyid@users.noreply.github.com>
2024-04-19 14:21:36 +0800

    Memanggil banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Mahasiswa ====================");
+  System.out.println("NIM  : 2210010614");
+  System.out.println("Nama : Muntahar Rosyid Ridlo");
+  System.out.println("======================================");
+ }
+}
